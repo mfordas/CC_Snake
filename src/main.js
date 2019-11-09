4982fc38a654634484428615a9a09888f8e31e29
Lukasz Kaczmarczyk <lukaszkaczmarczyk1996@gmail.com>
2019-11-09 19:52:27 +0100

    Ekran GameOver, kolizja ze scianami i wezem

--- src/main.js
+++ src/main.js
@@ -6,20 +6,51 @@ export const ctx = canvas.getContext('2d');
 export const cw = canvas.width / 20;
 export const ch = canvas.height / 20;
 
-const snake = new Snake(50, 50);
-const wall = new Wall(cw / 2, ch / 2);
+let snake = new Snake(50, 50);
+let wall = new Wall(cw / 2, ch / 2);
+let failed = false;
 
 const gameLoop = () => {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.width, canvas.height); //tło
   snake.move();
   snake.tailMove();
-  snake.onHit();
   snake.draw();
   wall.draw();
+  if (snake.onHit()) {
+    gameOver(); 
+    return; 
+  };
 
   requestAnimationFrame(gameLoop); // ta linijka musi być zawsze na końcu funkcji
 };
+
+//wyświetla ekran konca gry
+const gameOver = () => {
+  //Game over
+  let fontHeight = 50;
+  ctx.font = 50 + "px Arial";
+  let textGameOVer = "Game Over";
+  let textGameOverSize = ctx.measureText(textGameOVer);
+  ctx.fillText(textGameOVer, canvas.width/2 - textGameOverSize.width/2 , canvas.height/2);
+  //Press Space to restart
+  ctx.font = "20px Arial";
+  let textPressSpace = "Press Space to restart";
+  let textPressSpaceSize = ctx.measureText(textPressSpace);
+  ctx.fillText(textPressSpace, canvas.width/2 - textPressSpaceSize.width/2 , canvas.height/2 + fontHeight/1.5);
+  
+  failed = true;
+}
+
+//restartuje gre
+const gameRestart = () => {
+  failed = false;
+  //restart obiektów
+  snake = new Snake(50, 50);
+  wall = new Wall (cw /2, ch /2);
+  //restart petli gry
+  requestAnimationFrame(gameLoop);
+}
 
 document.addEventListener('keypress', ({ keyCode }) => {
   console.log(keyCode);
@@ -30,6 +61,10 @@ document.addEventListener('keypress', ({ keyCode }) => {
   if (keyCode === 83 || (keyCode == 115 && snake.direction != 'UP')) snake.setDirection('DOWN');
   //Klawisz "K" do wydłużania węża
   if (keyCode === 107) snake.expandSnake();
+  // Spacja resetuje gre, jeżeli przegrana
+  if (keyCode === 32) {
+    if(failed) gameRestart();
+  }  
 });
 
 requestAnimationFrame(gameLoop);
--- src/snake.js
+++ src/snake.js
@@ -1,4 +1,5 @@
-import { ctx, cw, ch } from './main';
+import { ctx, cw, ch, canvas } from './main';
+import { runInThisContext } from 'vm';
 
 class Snake {
   constructor(x, y) {
@@ -44,15 +45,56 @@ class Snake {
       this.y += this.speed;
     }
   }
+  
   onHit() {
     //check if hit sth
+    if (this.x < 0 || this.y < 0 || this.x + this.cell > canvas.width || this.y + this.cell > canvas.height)
+    {
+      return true;
+    }
+    //minimalna liczba czesci weza przy ktorej moze sie ugryzc
+    let minPartsNumber = Math.ceil((3*this.cell)/this.speed);
+    for (let i=0; i<this.tailLength - minPartsNumber; i++) {
+      //lewy gorny rogW
+      if (this.x > this.tail[i].x && 
+        this.x < this.tail[i].x + this.cell && 
+        this.y > this.tail[i].y && 
+        this.y < this.tail[i].y + this.cell) {
+          return true;
+      }
+      //prawy gorny rog
+      else if (this.x + this.cell > this.tail[i].x &&
+        this.x + this.cell < this.tail[i].x + this.cell && 
+        this.y > this.tail[i].y && 
+        this.y < this.tail[i].y + this.cell) {
+          return true;
+      }
+      //lewy dolny rog
+      else if (this.x > this.tail[i].x && 
+        this.x < this.tail[i].x + this.cell && 
+        this.y + this.cell > this.tail[i].y && 
+        this.y + this.cell < this.tail[i].y + this.cell) {
+          return true;
+      }
+      //prawy dolny rog
+      else if (this.x + this.cell > this.tail[i].x && 
+        this.x + this.cell < this.tail[i].x + this.cell && 
+        this.y + this.cell > this.tail[i].y && 
+        this.y +this.cell < this.tail[i].y + this.cell) {
+          return true;
+      }
+    } 
+    return false;
   }
+
   tailMove() {
     for (let i = 0; i < this.tail.length - 1; i++) {
       this.tail[i] = this.tail[i + 1];
     }
-    this.tail[this.tailLength] = { x: this.x, y: this.y };
+    this.tail[this.tailLength] = { x: this.x, y: this.y};
   }
+
+
 }
 
 export default Snake;
